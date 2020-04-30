967d491faf07b6e65c3d36bc99f367bfefc8765e
Heinrich <heinrich.ahrend@outlook.de>
2020-04-30 14:53:41 +0200

    uebung2 programmiert

--- uebung2.java
+++ uebung2.java
@@ -17,28 +17,26 @@ public class uebung2 extends PApplet
         size(500,500);
     }        
 
-    int[] weiss = {255,255,255};
-    int[] schwarz = {0,0,0};
     int[] hellblau = {7,176,216};
     int[] dunkelblau = {6,69,144};
     int s = 40;//Seitenlaenge grosse Quadrate
-    int k = 10;//Seitenlange kleines Viereck
+    int k = 10;//Breite kleines Quadrat
 
     @Override
     public void setup()
     {
-        background(weiss[0],weiss[1],weiss[2]);
+        background(255);
         schwarzeBalken();
-        blaueBalken();
+        blaueBalken_mitRauten();
     }
 
     public void schwarzeBalken()
     {
         int x = 0;
 
-        while (x<500){
-            fill (schwarz[0],schwarz[1],schwarz[2]);
-            rect (s/2+x,0,s,500);
+        while (x<width){
+            fill (0);
+            rect (x+s/2,0,s,500);
             x = x+2*s;
         }
     }
@@ -54,11 +52,13 @@ public class uebung2 extends PApplet
         return neueFarbe;   
     }
 
-    public void blaueBalken()
+    public void blaueBalken_mitRauten()
     {
         int x = 0;
         int y = s;
-        int[] farbe = dunkelblau;
+        int[] anfangsfarbe = dunkelblau;
+        int[] farbe = anfangsfarbe;
+        int rautenfarbe = 0;
 
         while (y<height){
             while (x<width){
@@ -66,33 +66,35 @@ public class uebung2 extends PApplet
 
                 fill (farbe[0],farbe[1],farbe[2]);
                 rect (x,y,s,s);
-
+                
+                vier_rauten(x+s/2,y,rautenfarbe);//Rauten oben
+                vier_rauten(x+s/2,y+s,255-rautenfarbe);//Rauten unten
+                
                 farbe = farbwechsel(farbe);
-                x = x+s;
-
-                fill (farbe[0],farbe[1],farbe[2]);
-                rect (x,y,s,s);
-                farbe = farbwechsel(farbe);
+                rautenfarbe = 255-rautenfarbe;
                 x = x+s;
             }
             x = 0;
             y = y+2*s+s/2;
-            farbe = farbwechsel(farbe);
+            anfangsfarbe = farbwechsel(anfangsfarbe);
         }
     }
 
-    public void viereck(int x1,int y1,int x2,int y2,int x3,int y3,int x4,int y4,int[]farbe)
+    public void viereck(int x1,int y1,int x2,int y2,int x3,int y3,int x4,int y4,int farbe)
     {
-        fill(farbe[0],farbe[1],farbe[2]);
+        fill(farbe);
         noStroke();
 
         quad(x1,y1,x2,y2,x3,y3,x4,y4);
 
     }
 
-    public void vier_rauten(int x,int y,int[]farbe)
+    public void vier_rauten(int x,int y,int farbe)
     {
-
+        viereck(x-k,y,x-k/2,y-k/2,x,y,x-k/2,y+k/2,farbe);//links
+        viereck(x-k/2,y-k/2,x,y-k,x+k/2,y-k/2,x,y,255-farbe);//oben
+        viereck(x,y,x+k/2,y+k/2,x+k,y,x+k/2,y-k/2,farbe);//rechts
+        viereck(x-k/2,y+k/2,x,y,x+k/2,y+k/2,x,y+k,255-farbe);//unten
     }   
 
     public static void main(String _args[]){ 
